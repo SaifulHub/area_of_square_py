9201386dbc39f0d3efac3d01902f388f870b6d90
SaifulHub <si.saiful.sist@gmail.com>
2021-05-07 21:46:43 +0600

    pycode for area of square

---
+++ areaofSquare.py
@@ -0,0 +1,5 @@
+length= int(input("enter the value of length of square : "))
+area = length*length
+circumference=length+length+length+length
+print("The Circumference of your square is:",circumference)
+print("The area of your square is: ",area)
